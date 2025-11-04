6b6de5266f6c49cd977cb402d342054907c25853
TRISTAN <debeaunetristan@gmail.com>
2025-11-04 15:17:07 +0100

    full of modifications yeah

--- Assets/Scripts/MaterialManager.cs
+++ Assets/Scripts/MaterialManager.cs
@@ -1,16 +1,38 @@
+using System.ComponentModel;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MaterialManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField]
+    Camera _camera;
+
+    [SerializeField]
+    Vector2 mousePosition2D;
+
+    private Material _material;
+    private Renderer _renderer;
+
+    private void Awake()
     {
-        
+        _camera = Camera.main;
+        _renderer = GetComponent<Renderer>();
+        _material = _renderer.material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
+
+        // screen position of the object.
+        Vector2 objectScreenPosition2D = _camera.WorldToScreenPoint(transform.position);
+
+        _material.SetVector("_MouseScreenPosition", mousePosition2D);
+        _material.SetVector("_objectScreenPosition", objectScreenPosition2D);
     }
 }
